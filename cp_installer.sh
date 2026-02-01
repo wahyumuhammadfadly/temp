245db2ae326cce1e2e2ff566b80b34d047e36193
wahyumuhammadfadly <78024091+wahyumuhammadfadly@users.noreply.github.com>
2026-02-01 09:53:20 +0700

    Update cp_installer.sh

--- cp_installer.sh
+++ cp_installer.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
+# Simpan ini di GitHub/Gist, lalu ambil URL RAW-nya.
 
-# Tangkap password dari argumen pertama
+# 1. Tangkap Password dari PHP
 ROOT_PASS=$1
 
-# 1. Setup Password Root & SSH (Agar bisa login PuTTY)
+# 2. Setup Password Root & SSH
 echo "root:$ROOT_PASS" | chpasswd
 sed -i 's/^#PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sed -i 's/^PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sed -i 's/^PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 service ssh restart
 
-# 2. Setup SWAP 2GB (Wajib untuk CloudPanel agar tidak crash saat install)
+# 3. Buat SWAP 2GB (Wajib untuk CloudPanel)
 fallocate -l 2G /swapfile
 chmod 600 /swapfile
 mkswap /swapfile
 swapon /swapfile
 echo '/swapfile none swap sw 0 0' >> /etc/fstab
 
-# 3. Update System
+# 4. Update & Install CloudPanel
 apt-get update -y
 apt-get install -y curl wget sudo
-
-# 4. Install CloudPanel (Database Engine: MariaDB 10.11)
 curl -sS https://installer.cloudpanel.io/ce/v2/install.sh -o install.sh
 echo "Installing CloudPanel..."
 sudo DB_ENGINE=MARIADB_10.11 bash install.sh
